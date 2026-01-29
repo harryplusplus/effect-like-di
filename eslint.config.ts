71a8f473fd0eb603af4cdd5d7b0631edd9b2c9d9
harryplusplus <harryplusplus@gmail.com>
2026-01-29 15:57:21 +0900

    chore: Initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,61 @@
+import path from 'node:path'
+
+import { includeIgnoreFile } from '@eslint/compat'
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import importPlugin from 'eslint-plugin-import'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
+
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore')
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: { allowDefaultProject: [] },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      'max-lines': [
+        'error',
+        { max: 300, skipBlankLines: true, skipComments: true },
+      ],
+      'max-lines-per-function': [
+        'error',
+        { max: 50, skipBlankLines: true, skipComments: true, IIFEs: true },
+      ],
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    plugins: { import: importPlugin },
+    rules: { 'import/extensions': ['error', 'never', { ts: 'always' }] },
+  },
+  prettierRecommended,
+])
---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'prettier'
+
+const config: Config = {
+  semi: false,
+  singleQuote: true,
+  objectWrap: 'collapse',
+}
+
+export default config
